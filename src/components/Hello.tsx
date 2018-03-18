3d3dc64d888bb66092857a1bb3fe28284496017c
Steven Cramer <Steven.Cramer@LceCorp.com>
2018-03-18 15:32:26 +0000

    Add Hello.tsx component

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -1,64 +1,72 @@
-// src/components/Hello.tsx
-import * as React from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import {
+  Button,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
 
-export interface Props {
+interface Props {
   name: string;
-  enthusiasmLevel?: number;
-  onIncrement?: () => void;
-  onDecrement?: () => void;
 }
 
-function Hello({ name, enthusiasmLevel = 1, onIncrement, onDecrement }: Props) {
-  if (enthusiasmLevel <= 0) {
-    throw new Error('You could be a little more enthusiastic. :D');
-  }
+interface State {
+  date: Date;
+}
 
-  return (
-    <View style={styles.root}>
+export class Hello extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {date: new Date()};
+  }
+  private updateDate = () => {
+    debugger;
+    this.setState({
+      date: new Date()
+    })
+  };
+
+  render() {
+    const { name, } = this.props;
+
+    return (
+      <View style={styles.root}>
         <Text style={styles.greeting}>
-            Hello {name + getExclamationMarks(enthusiasmLevel)}
+          Hello {name + '!'}
+        </Text>
+        <Text style={styles.greeting}>
+          Current Time: {this.state.date.toLocaleString()}
         </Text>
         <View style={styles.buttons}>
-            <View style={styles.button}>
-                <Button title="-" onPress={onDecrement || (() => {})} color='red' />
-            </View>
-            <View style={styles.button}>
-                <Button title="+" onPress={onIncrement || (() => {})} color='blue' />
-            </View>
+          <View style={styles.button}>
+            <Button title="Update Time" onPress={this.updateDate} accessibilityLabel="decrement" color='red' />
+          </View>
         </View>
-    </View>
-  );
+      </View>
+    );
+  }
 }
-
-export default Hello;
 
 // styles
 
 const styles = StyleSheet.create({
-    root: {
-        alignItems: "center",
-        alignSelf: "center"
-    },
-    buttons: {
-        flexDirection: "row",
-        minHeight: 70,
-        alignItems: "stretch",
-        alignSelf: "center",
-        borderWidth: 5,
-    },
-    button: {
-        flex: 1,
-        paddingVertical: 0,
-    },
-    greeting: {
-        color: "#999",
-        fontWeight: "bold"
-    }
+  root: {
+      alignItems: "center",
+      alignSelf: "center"
+  },
+  buttons: {
+      flexDirection: "row",
+      minHeight: 70,
+      alignItems: "stretch",
+      alignSelf: "center",
+      borderWidth: 5,
+  },
+  button: {
+      flex: 1,
+      paddingVertical: 0,
+  },
+  greeting: {
+      color: "#999",
+      fontWeight: "bold"
+  }
 });
-
-// helpers
-
-function getExclamationMarks(numChars: number) {
-  return Array(numChars + 1).join('!');
-}
--- src/components/__tests__/Hello.tsx
+++ src/components/__tests__/Hello.tsx
@@ -1,10 +1,13 @@
-import * as React from 'react';
+import React from 'react';
 import { Text } from 'react-native';
-import { shallow } from 'enzyme';
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 
-import Hello from '../Hello';
+import { Hello } from '../Hello';
+
+configure({ adapter: new Adapter() });
 
 it('renders correctly with defaults', () => {
     const hello = shallow(<Hello name="World" />);
-    expect(hello.find(Text).render().text()).toEqual("Hello World!");
+    expect(hello.find(Text).first().render().text()).toEqual("Hello World!");
 })
